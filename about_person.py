cb4386918f7f5ccac590a2d4aa4038695b3a05cc
Valeentin87 <valeentin87@list.ru>
2023-10-02 23:25:12 +0300

    реализовал логику проверку на наличие номера телефона во введенных данных

--- about_person.py
+++ about_person.py
@@ -9,9 +9,30 @@
 flag = True
 while flag:
     try:
-        person_data = input('введите через пробел следующие данные о себе: Фамилия Имя Отчество датарождения номертелефона пол и нажмите Enter')
+        person_data = input('введите через пробел следующие данные о себе: Фамилия Имя Отчество датарождения номертелефона пол и нажмите Enter: ')
         if person_data == "":
             raise Exception("вы не ввели данные!!! Попробуйте снова!!!")
-        flag = False
+        person_data_list = person_data.split()
+        print(person_data_list)
+        
+        if len(person_data_list)>6:
+            raise Exception("вы ввели данных больше, чем требуется: проверьте и введите заново: ")
+        elif len(person_data_list)<6:
+            raise Exception("вы ввели данных меньше, чем требуется: проверьте и введите заново: ")
+        
+        else: 
+            print("Вы ввели корректные данные, спасибо!!!")
+            flag = False
+        
+        
     except Exception as e:
         print(e)
+
+def check_phone_number(data_person):
+    for data in data_person:
+        if data.isdigit():
+            return data, True
+    return "номер телефона в введенных данных отсутствует", False
+
+print(check_phone_number(person_data_list))
+    
