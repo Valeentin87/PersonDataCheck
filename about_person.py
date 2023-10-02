c0c9c17d0ce26646dbfb9bd714ea13c84fcfce34
Valeentin87 <valeentin87@list.ru>
2023-10-02 23:49:03 +0300

    реализовал логику поиска даты рождения во введенных данных

--- about_person.py
+++ about_person.py
@@ -34,5 +34,15 @@ def check_phone_number(data_person):
             return data, True
     return "номер телефона в введенных данных отсутствует", False
 
+def check_date_of_birth(data_person):
+    for data in data_person:
+        if len(data) == 10 and data[2] == '.' and data[5] == '.':
+            if 0<int(data[0:2])<=31:
+                if 0<int(data[3:5])<=12:
+                    if int(data[6:]):
+                        return data, True
+    return "дата рождения во введенных данных отсутствует или введена не корректно!!! " 
+
 print(check_phone_number(person_data_list))
+print(check_date_of_birth(person_data_list))
     
