ef60908366d9676a895759a42336438d38ec37a6
Valeentin87 <valeentin87@list.ru>
2023-10-03 02:27:21 +0300

    доработал логику обработки входных данных

--- about_person.py
+++ about_person.py
@@ -12,8 +12,8 @@ from Person import *
 flag = True
 while flag:
     try:
-        #person_data = input('введите через пробел следующие данные о себе: Фамилия Имя Отчество датарождения номертелефона пол и нажмите Enter: ')
-        person_data = "Иванов Иван Иванович 89131840501 23.01.2016 f"
+        person_data = input('введите через пробел следующие данные о себе: Фамилия Имя Отчество датарождения номертелефона пол и нажмите Enter: ')
+        #person_data = "Иванов Иван Иван7вич 8913184051 23.07.2012 f"
         if person_data == "":
             raise Exception("вы не ввели данные!!! Попробуйте снова!!!")
         person_data_list = person_data.split()
@@ -24,37 +24,38 @@ while flag:
         elif len(person_data_list)<6:
             raise Exception("вы ввели данных меньше, чем требуется: проверьте и введите заново: ")
         elif check_date_of_birth(person_data_list)[1] == False:
-            raise Exception(check_date_of_birth(person_data_list)[0])
+            raise Exception("Во входных данных не корректная дата рождения!!!")
         elif check_gender(person_data_list)[1] == False:
-            raise Exception(check_gender(person_data_list)[0])
+            raise Exception("Во входных данных не корректный пол")
         elif check_patronymic(person_data_list)[1] == False:
-            raise Exception(check_patronymic(person_data_list)[0])
+            raise Exception("Во входных данных не корректное отчество")
         elif check_phone_number(person_data_list)[1] == False:
-            raise Exception(check_phone_number(person_data_list)[0])
+            raise Exception("Во входных данных не корректный номер телефона")
         elif check_surname(person_data_list)[1] == False:
-            raise Exception(check_surname(person_data_list)[0])
+            raise Exception("Во вхожных данных не корректная фамилия!!! ")
         
         else: 
             print("Вы ввели корректные данные, спасибо!!!")
+            name_person = check_name(person_data_list.copy(), check_phone_number(person_data_list),check_date_of_birth(person_data_list), \
+                 check_gender(person_data_list), check_surname(person_data_list), check_patronymic(person_data_list))
+            firt_Person =  Person(check_surname(person_data_list)[0],name_person[0],check_patronymic(person_data_list)[0],check_phone_number(person_data_list)[0], \
+                      check_date_of_birth(person_data_list)[0],check_gender(person_data_list)[0]) 
+            print(firt_Person)
             flag = False
         
         
     except Exception as e:
         print(e)
 
-name_person = check_name(person_data_list.copy(), check_phone_number(person_data_list),check_date_of_birth(person_data_list), \
-                 check_gender(person_data_list), check_surname(person_data_list), check_patronymic(person_data_list))
-firt_Person =  Person(check_surname(person_data_list)[0],name_person[0],check_patronymic(person_data_list)[0],check_phone_number(person_data_list)[0], \
-                      check_date_of_birth(person_data_list)[0],check_gender(person_data_list)[0]) 
-print(firt_Person)
-'''
+
+
 print(check_phone_number(person_data_list))
 print(check_date_of_birth(person_data_list))
 print(check_gender(person_data_list))
 print(check_surname(person_data_list))
 print(check_patronymic(person_data_list))
 
-'''
+
 
 
 
